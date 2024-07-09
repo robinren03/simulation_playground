277c8251f864de39c31951388a91e0a1b843f129
Yanyu Ren <renyanyu323@163.com>
2024-07-09 16:00:43 +0800

    Better test_overlap

--- test_overlap.py
+++ test_overlap.py
@@ -17,18 +17,18 @@ torch_profiler = profile(
 torch.distributed.init_process_group('nccl')
 rank = torch.distributed.get_rank()
 num_ranks = torch.distributed.get_world_size()
-simpleimc.torch.ops.init(rank, num_ranks, -1, 0)
+# simpleimc.torch.ops.init(rank, num_ranks, -1, 0)
 device = torch.device('cuda', rank)
 print(f"rank {rank} reporting")
 dtype=torch.float
 dim = 12288
 batch_size = 4
 seq_len = 2048
-micro_iter = 8
+micro_iter = 1
 iter = 20
-# comm_type = "allreduce"
+comm_type = "allreduce"
 # comm_type = "reduce_scatter"
-comm_type = "allgather"
+# comm_type = "allgather"
 
 def get_input() -> torch.Tensor:
     return torch.randn(batch_size, seq_len, dim, device=device, dtype=dtype)
@@ -70,41 +70,71 @@ class MatMulNccl(nn.Module):
         if comm_type in ["allreduce"]:
             y = torch.concat(y)
         torch.distributed.barrier()
+        time_pool = 0
         for i in range(micro_iter):
             if comm_type == "allreduce":
-                handle = torch.distributed.all_reduce(y, op=torch.distributed.ReduceOp.AVG, async_op=True)
+                comm_stream = torch.cuda.Stream()
+                with torch.cuda.stream(comm_stream):
+                    handle = torch.distributed.all_reduce(y, op=torch.distributed.ReduceOp.AVG, async_op=True)
             elif comm_type == "allgather":
                 handle = torch.distributed._all_gather_base(self.buffer, y[rank], async_op=True)
             else:
                 handle = torch.distributed._reduce_scatter_base(y[rank], self.buffer, async_op=True)
+            # handle.wait()
+            # torch.distributed.barrier()
+            
+            # compute_stream = torch.cuda.Stream()
+            # with torch.cuda.stream(compute_stream):
+            # start = torch.cuda.Event(enable_timing = True)
+            # end = torch.cuda.Event(enable_timing = True)
+            # start.record()
             x = self.fc(x)
+            # end.record()
+            # end.synchronize()
+
+            # time_pool += start.elapsed_time(end)
             handle.wait()
+            torch.distributed.barrier()
+        return time_pool
 
 
 def main():
     if rank == 0:
-        nsys_filename = f"/opt/tiger/tmp.nsys_trace_{comm_type}_{int(time.time())}.qdrep"
-        time.sleep(10)
-        # torch_profiler.start()
+        nsys_filename = f"/data2/ryanyu/repos/simulation_playground/tmp.nsys_trace_{comm_type}_{int(time.time())}_{rank}.qdrep"
+    #     # time.sleep(10)
+    #     torch_profiler.start()
+    #     time.sleep(10)
+
+    time_pool = 0
     for i in range(iter):
-        if rank == 0:
+        if i==10 and rank == 0:
             print(f"iter {i}")
             if i == 10:
                 os.system(f"nsys start -o {nsys_filename}")
             elif i == 13:
                 os.system("nsys stop")
-        pure_matmul = PureMatMut(dim).to(device)
-        pure_nccl = PureNccl().to(device)
+            # if rank==0: 
+                # torch_profiler.start()
+            time.sleep(10)
+        
+        torch.distributed.barrier()
+        # pure_matmul = PureMatMut(dim).to(device)
+        # pure_nccl = PureNccl().to(device)
         matmul_nccl = MatMulNccl(dim).to(device)
         x = get_input()
         y = [get_input() for i in range(num_ranks)]
         # pure_matmul(x)
         # pure_nccl(y)
-        matmul_nccl(x, y)
+        p = matmul_nccl(x, y)
+        if (i>10):
+            print(f"{rank} {i} {p}") 
+            time_pool += p
         
+    
+    print(f"{rank} reported time: {time_pool} ms")
     # if rank == 0:
         # torch_profiler.stop()
-        # filename = f"/opt/tiger/tmp.torch_trace_{time.time()}.json"
+        # filename = f"/miaoyk/yanyu_playground/simulation_playground/tmp.torch_trace_{time.time()}_{rank}.json"
         # print(f"==== begin to dump {filename} ====")
         # torch_profiler.export_chrome_trace(filename)
         # print(f"==== succesfully dumped {filename} ====")
